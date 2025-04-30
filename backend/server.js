fae936681e84da4553759bfd729485fb5b8d6ba4
mohitkumar <mohitkumar269856@gmail.com>
2025-04-30 14:07:36 +0530

    update deploy files

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
+// Deploy Settings
+import path from "path";
+const __dirname = path.resolve();
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -14,6 +17,11 @@ app.get("/", (req, res) => {
   });
 });
 
+// Deploy Settings
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
 app.listen(process.env.PORT, () => {
   console.log(`Server listening at port ${process.env.PORT}`);
 });
